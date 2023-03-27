18031341c3ba8083def62dbe7ea1480bfecaf4dc
Trainer-Learn-to-Code <80373993+Trainer-Learn-to-Code@users.noreply.github.com>
2023-03-27 22:47:25 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/sreelakshmi/google_signin_integration/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sreelakshmi.google_signin_integration
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/profile_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:google_signin_integration/screens/login_page.dart';
+
+import '../authentication/google_signin_page.dart';
+
+class Profile_Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.blue, Colors.grey],
+          ),
+        ),
+        child: Center(child: Text("Home")),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            DrawerHeader(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  imageUrl!,
+                ),
+                radius: 100,
+                backgroundColor: Colors.transparent,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'NAME',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.black54),
+              ),
+              subtitle: Text(
+                name!,
+                style: TextStyle(
+                    color: Colors.deepPurple),
+              ),
+            ),
+            ListTile(
+              subtitle: Text(
+                email!,
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                ),
+              ),
+              title: Text(
+                'EMAIL',
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Divider(
+
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                signOutGoogle();
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (context) {
+                  return LoginPage();
+                }), ModalRoute.withName('/'));
+              },
+              title: Text(
+                'Sign Out',
+                style: TextStyle(color: Colors.black),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
